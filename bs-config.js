95dd6acfdc26fa2ac1dce4345429397acc680eaf
muhammadnurulahsan <mdnurulahsanbdbd@gmail.com>
2026-09-13 12:04:43 +0600

    Add gitignore, npm setup, coupon & pricing modules

--- bs-config.js
+++ bs-config.js
@@ -3,10 +3,10 @@ module.exports = {
   files: [
     "C:/xampp/htdocs/wordpress/wp-content/plugins/iqu-registration/**/*.php",
     "C:/xampp/htdocs/wordpress/wp-content/plugins/iqu-registration/**/*.css",
-    "C:/xampp/htdocs/wordpress/wp-content/plugins/iqu-registration/**/*.js"
+    "C:/xampp/htdocs/wordpress/wp-content/plugins/iqu-registration/**/*.js",
   ],
   watchOptions: {
     usePolling: true,
-    interval: 500
-  }
+    interval: 1000,
+  },
 };
